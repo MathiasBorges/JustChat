ac5922a094027cfab3c56618de77749f1d689dbe
MathiasBorges <dsmathiasmarques@gmail.com>
2025-03-17 17:21:14 -0300

    updating Sala for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { Sala } from "./pages/Sala";
-import { SignIn } from "./pages/SignIn";
+import Sala from "./pages/Sala";
+import SignIn from "./pages/SignIn";
 import { Account } from "appwrite";
 import client from "./WriteConfig";
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Pode ajustar se necessário
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
